16de9872b7845dc2f98a979d64cef0691a4083cf
Nihar Sawant <nihar@expojure.com>
2014-07-09 19:56:01 +0530

    Removed bower dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,7 @@ module.exports = function (grunt) {
     less : {
       compile : {
         files : {
-          "build/base.css" : [
-            "bower_components/normalize-css/normalize.css",
-            "src/base.less"
-          ],
+          "build/base.css" : "src/base.less",
           "build/buttons.css" : "src/buttons.less"
         }
       }
@@ -34,24 +31,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Created a custom task to install Bower Dependencies
-  grunt.registerTask('bower_install', 'Installs Bower dependencies.', function () {
-    var bower = require('bower'), done  = this.async();
-
-    bower.commands.install()
-      .on('log', function (data) {
-        if (data.id !== 'install') { return; }
-        grunt.log.writeln('bower ' + data.id.cyan + ' ' + data.message);
-      })
-      .on('end', function (results) {
-        if (!Object.keys(results).length) {
-          grunt.log.writeln('No bower packages to install.');
-        }
-
-        done();
-      });
-  });
-
-  grunt.registerTask('default', ['bower_install', 'less:compile']);
+  grunt.registerTask('default', ['less:compile']);
 
 };
